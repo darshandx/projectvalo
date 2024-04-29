9dd859fc628f30cc187f7e49c2df0bdbc069f42b
darshandx <131342131+darshandx@users.noreply.github.com>
2024-04-29 13:58:08 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,18 +1,1 @@
-public class main {
-    public static int calctPow(int x, int n){
-        if(n==0){
-            return 1;
-        }
-        if(x==0){
-            return 0;
-        }
-        int xPownm1=calctPow(x, n-1);
-        int xPown= x * xPownm1;
-        return xPown;
-    }
-    public static void main(String[] args) {
-       int x=2, n=5;
-       int ans=calctPow(x, n);
-       System.out.println(ans);
-    }
-}
+
